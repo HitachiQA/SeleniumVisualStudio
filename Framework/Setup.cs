f1488068cbc294d46849230bdf1d6a6875c2f389
Swetha <swetha.ojili@hitachiconsulting.com>
2017-09-11 13:02:49 +0530

    First commit

---
+++ Framework/Setup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Setup
+    {
+        static IWebDriver driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            WebUtility webUtil = new WebUtility();
+            webUtil.log("Sample text");
+        }
+    }
+}
---
+++ Framework/Utilities/LoggerUtility.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class LoggerUtility
+    {
+        public static String filename = "log.txt";
+        public static String file = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) + "\\" + filename;
+        //public FileStream fstream;
+
+        public LoggerUtility()
+        {
+            //Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            //Console.WriteLine(Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())));
+            Console.WriteLine(file);
+            try
+            {
+                if (!File.Exists(file))
+                {
+                    File.Create(file);
+                    //fstream = new FileStream(file, FileMode.Create);
+                    //TextWriter tw = new StreamWriter(filePath);
+                    Console.WriteLine("Empty Log File is created");
+
+                }
+                else
+                {
+                    File.WriteAllText(file, string.Empty);
+                    //fstream = new FileStream(file, FileMode.Open);
+                    Console.WriteLine("Log File already exist, contents are cleared.");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        public String returnFilePath()
+        {
+            return file;
+        }
+
+
+
+
+}
+
